f7fda1efa6d01783e88c16140f1f254e5c01bde6
m-h-thomas <mhthomas.8193@gmail.com>
2025-02-06 22:19:34 -0700

    new tasks are added successfully with a checkbox to mark complete. working on using checkbox to move current task to completed task

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ let completedTasks = [];
 /* Dynamically displays items from allItems array */
 const displayTasks = () => {
     let currentTasksList = document.querySelector('.currentTaskList');
-    let allCurrentTasks = currentTasks.map((elm) => `<li class="currentTaskList">${elm}</li>`)
+    let allCurrentTasks = currentTasks.map((elm) => `<li class="currentTaskList">${elm}<input class="checkbox" type="checkbox"></li>`)
     .reduce((acc, elm) => acc += elm, '');
     currentTasksList.innerHTML = allCurrentTasks;
+
+    // let completedTasksList = document.querySelector('.currentTaskList');
+    // let allCompletedTasks = completedTasks.map((elm) => `<li class="completedTaskList">${elm}</li>`).reduce((acc, elm) => acc += elm, '')
+    // completedTasksList.innerHTML = allCompletedTasks;
   };
 
 /* Grabs the content of user-input-field and passes it button function */
@@ -27,6 +31,19 @@ const getInputValue = () => {
         currentTasks.push(userInput);
         console.log(userInput);
         input.value = '';
+        displayTasks();
+    })
+}
+
+
+/* Grabs content from current tasks and passes it completed tasks */
+const completeTask = () => {
+    // let complete = document.querySelector('.currentTaskList').checked = true;
+    let complete = document.querySelector('.checkbox');
+    complete.addEventListener('click', () => {
+        if(complete.checked == true){
+            completedTasks.push(complete);
+        }
         displayTasks();
     })
 }
