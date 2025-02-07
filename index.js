1cb018c9553e2b4a4a49ef9c26a71ddd76714bcc
m-h-thomas <mhthomas.8193@gmail.com>
2025-02-06 21:28:00 -0700

    button grabs value of user input field now

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 console.log('My code is running.')
+
+/* Global variables */
+let userInput = '';
+let currentTasks = [];
+let completedTasks = [];
+
+
+/* Dynamically displays items from allItems array */
+const displayTasks = () => {
+    let currentTasksList = document.querySelector('#new-task-list');
+    let allCurrentTasks = currentTasks.map((elm) => `<div class="newTasks><li><button class="addTask">${elm}</button></li></div`)
+    .reduce((acc, elm) => acc += elm, '');
+    currentTasksList.innerHTML = allCurrentTasks;
+  };
+
+/* Grabs the content of user-input-field and passes it button function */
+const getInputValue = () => {
+    let input = document.querySelector('#user-input-field')
+    let addButton = document.querySelector('#add-task-button') 
+
+    input.addEventListener('input', (eventObject) => {
+        userInput = eventObject.target.value;
+    });
+
+    addButton.addEventListener('click', () => {
+        currentTasks.push(userInput);
+        console.log(userInput);
+        input.value = '';
+        displayTasks();
+    })
+}
+
+getInputValue();
